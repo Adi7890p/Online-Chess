7c0cf280f7d74f510020860ce08f6e4edbc83d24
Aditya kuril <113298694+Adi7890p@users.noreply.github.com>
2025-06-07 18:22:58 +0530

    Update connection.js

--- public/connection.js
+++ public/connection.js
@@ -14,6 +14,7 @@ function connect(room) {
 
   socket.onmessage = (e) => {
     const data = JSON.parse(e.data);
+    
     if (data.type === "move") {
     const square = data.square;
     if (window.boardHandlers[square]) {
@@ -25,14 +26,23 @@ function connect(room) {
 function sendMove(squareId) {
   socket.send(JSON.stringify({ type: "move", roomCode, square: squareId }));
 }
+
 document.getElementById('hostBtn').onclick = () => {
   roomCode = randomCode();
   document.getElementById('roomDisplay').innerText = `Room Code: ${roomCode}`;
   connect(roomCode);
+  if (data.type === 'start') {
+      localStorage.setItem('roomCode', room);
+      window.location.href = 'p1.html';
+    }
 };
 
 
 document.getElementById('joinBtn').onclick = () => {
   roomCode = document.getElementById('roomInput').value.trim().toUpperCase();
   connect(roomCode);
+  if (data.type === 'start') {
+      localStorage.setItem('roomCode', room);
+      window.location.href = 'p2.html';
+    }
 };
